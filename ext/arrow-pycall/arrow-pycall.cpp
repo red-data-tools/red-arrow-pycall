b969ae773128e9a22a960ced29f7867b8c1a924f
Kouhei Sutou <kou@clear-code.com>
2017-07-10 17:49:38 +0900

    Simplify

--- ext/arrow-pycall/arrow-pycall.cpp
+++ ext/arrow-pycall/arrow-pycall.cpp
@@ -20,30 +20,28 @@
 
 #include <arrow/python/pyarrow.h>
 
-template<bool long_size_is_greater_than_pointer_size>
-struct pointer2value;
+namespace {
+  template<bool long_size_is_greater_than_pointer_size>
+  struct ObjectConverter;
 
-template<>
-struct pointer2value<true> {
-  VALUE operator()(void *pointer)
-  {
-    return LONG2NUM(reinterpret_cast<long>(pointer));
+  template<>
+  struct ObjectConverter<true> {
+    VALUE to_ruby(void *pointer) {
+      return LONG2NUM(reinterpret_cast<long>(pointer));
+    }
+  };
+
+  template<>
+  struct ObjectConverter<false> {
+    VALUE to_ruby(void *pointer) {
+      return LL2NUM(reinterpret_cast<LONG_LONG>(pointer));
+    }
+  };
+
+  VALUE pointer_to_ruby(void *pointer) {
+    ObjectConverter<(sizeof(long) >= sizeof(void *))> converter;
+    return converter.to_ruby(pointer);
   }
-};
-
-template<>
-struct pointer2value<false> {
-  VALUE operator()(void *pointer)
-  {
-    return LL2NUM(reinterpret_cast<LONG_LONG>(pointer));
-  }
-};
-
-static inline VALUE
-PTR2NUM(void *pointer)
-{
-  pointer2value<(sizeof(long) >= sizeof(void *))> converter;
-  return converter(pointer);
 }
 
 extern "C" void Init_arrow_pycall(void);
@@ -54,7 +52,7 @@ rb_arrow_buffer_to_python_object_pointer(VALUE self)
   auto buffer = GARROW_BUFFER(RVAL2GOBJ(self));
   auto arrow_buffer = garrow_buffer_get_raw(buffer);
   auto py_buffer = arrow::py::wrap_buffer(arrow_buffer);
-  return PTR2NUM(py_buffer);
+  return pointer_to_ruby(py_buffer);
 }
 
 static VALUE
@@ -63,7 +61,7 @@ rb_arrow_data_type_to_python_object_pointer(VALUE self)
   auto data_type = GARROW_DATA_TYPE(RVAL2GOBJ(self));
   auto arrow_data_type = garrow_data_type_get_raw(data_type);
   auto py_data_type = arrow::py::wrap_data_type(arrow_data_type);
-  return PTR2NUM(py_data_type);
+  return pointer_to_ruby(py_data_type);
 }
 
 static VALUE
@@ -72,7 +70,7 @@ rb_arrow_field_to_python_object_pointer(VALUE self)
   auto field = GARROW_FIELD(RVAL2GOBJ(self));
   auto arrow_field = garrow_field_get_raw(field);
   auto py_field = arrow::py::wrap_field(arrow_field);
-  return PTR2NUM(py_field);
+  return pointer_to_ruby(py_field);
 }
 
 static VALUE
@@ -81,7 +79,7 @@ rb_arrow_schema_to_python_object_pointer(VALUE self)
   auto schema = GARROW_SCHEMA(RVAL2GOBJ(self));
   auto arrow_schema = garrow_schema_get_raw(schema);
   auto py_schema = arrow::py::wrap_schema(arrow_schema);
-  return PTR2NUM(py_schema);
+  return pointer_to_ruby(py_schema);
 }
 
 static VALUE
@@ -90,7 +88,7 @@ rb_arrow_array_to_python_object_pointer(VALUE self)
   auto array = GARROW_ARRAY(RVAL2GOBJ(self));
   auto arrow_array = garrow_array_get_raw(array);
   auto py_array = arrow::py::wrap_array(arrow_array);
-  return PTR2NUM(py_array);
+  return pointer_to_ruby(py_array);
 }
 
 static VALUE
@@ -99,7 +97,7 @@ rb_arrow_tensor_to_python_object_pointer(VALUE self)
   auto tensor = GARROW_TENSOR(RVAL2GOBJ(self));
   auto arrow_tensor = garrow_tensor_get_raw(tensor);
   auto py_tensor = arrow::py::wrap_tensor(arrow_tensor);
-  return PTR2NUM(py_tensor);
+  return pointer_to_ruby(py_tensor);
 }
 
 static VALUE
@@ -108,7 +106,7 @@ rb_arrow_column_to_python_object_pointer(VALUE self)
   auto column = GARROW_COLUMN(RVAL2GOBJ(self));
   auto arrow_column = garrow_column_get_raw(column);
   auto py_column = arrow::py::wrap_column(arrow_column);
-  return PTR2NUM(py_column);
+  return pointer_to_ruby(py_column);
 }
 
 static VALUE
@@ -117,7 +115,7 @@ rb_arrow_table_to_python_object_pointer(VALUE self)
   auto table = GARROW_TABLE(RVAL2GOBJ(self));
   auto arrow_table = garrow_table_get_raw(table);
   auto py_table = arrow::py::wrap_table(arrow_table);
-  return PTR2NUM(py_table);
+  return pointer_to_ruby(py_table);
 }
 
 static VALUE
@@ -126,7 +124,7 @@ rb_arrow_record_batch_to_python_object_pointer(VALUE self)
   auto record_batch = GARROW_RECORD_BATCH(RVAL2GOBJ(self));
   auto arrow_record_batch = garrow_record_batch_get_raw(record_batch);
   auto py_record_batch = arrow::py::wrap_record_batch(arrow_record_batch);
-  return PTR2NUM(py_record_batch);
+  return pointer_to_ruby(py_record_batch);
 }
 
 extern "C" void
